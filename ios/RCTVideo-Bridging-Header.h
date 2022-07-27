28a1be594cf6da5a76a5eadd9ad7b722745e683f
Oleg Koptev <olegstankoptev@gmail.com>
2022-07-27 09:11:07 +0300

    added bridging header

---
+++ ios/RCTVideo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RCTVideo-Bridging-Header.h
+//  RCTVideo
+//
+//  Created by Oleg Koptev on 27.07.2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#ifndef RCTVideo_Bridging_Header_h
+#define RCTVideo_Bridging_Header_h
+
+
+#endif /* RCTVideo_Bridging_Header_h */
